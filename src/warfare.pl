f3430531a1e0bd49cf6d8d3841371a1817d3806c
Mike Dunhem <mikael.dunhem@gmail.com>
2017-06-28 16:00:59 -0400

    Add ability to read command line args

--- src/warfare.pl
+++ src/warfare.pl
@@ -3,4 +3,39 @@
 % Assignment 2 - THE CALCULUS OF CONVENTIONAL WAR: DYNAMIC ANALYSIS WITHOUT LANCHESTER THEORY
 
 main :-
-    write('Hello World!'), nl, halt.
+    (
+        current_prolog_flag(os_argv, Argv) -> true;
+        current_prolog_flag(argv, Argv)
+    ),
+    append([_, _, _, _, _, _], Rest, Argv),
+    (
+        Rest = [Command|SubArgs] -> main(Command, SubArgs);
+        usage
+    ).
+
+main(blah, Rest) :-
+    (
+        Rest = ['--all'] ->
+            banner, write('Hello All!'), nl;
+        Rest = ['--missing'] ->
+            banner, write('Hello Missing!'), nl;
+        Rest = [] ->
+            banner, test('Blah'), nl
+    ).
+
+main(_, _) :- !, usage.
+
+test(X) :-
+    write(X).
+
+banner :-
+    writeln('------------------------------------------------------------------------------'),
+    writeln(' THE CALCULUS OF CONVENTIONAL WAR: DYNAMIC ANALYSIS WITHOUT LANCHESTER THEORY '),
+    writeln('                Mike Dunhem - CS 355, Summer 2017, Project 2                  '),
+    writeln('------------------------------------------------------------------------------'),
+    writeln('').
+
+usage :-
+    banner,
+    writeln(' Usage: warfare blah [--all | --missing]'),
+    writeln('        warfare').
